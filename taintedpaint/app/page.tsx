386b4bbab2ff9dcd467ab70fff380bcce9fe7ab1
SpaceshipxDev <150497663+SpaceshipxDev@users.noreply.github.com>
2025-07-30 19:41:53 +0800

    Revert "Add SQLite-based login"

--- taintedpaint/app/page.tsx
+++ taintedpaint/app/page.tsx
@@ -1,14 +1,9 @@
 // app.page.tsx 
 
+"use client"
+
 import KanbanBoard from "@/kanban-board"
-import { cookies } from 'next/headers'
-import { getUserBySession } from '@/lib/userStore'
-import { redirect } from 'next/navigation'
 
 export default function Page() {
-  const cookieStore = cookies()
-  const token = cookieStore.get('session')?.value
-  const user = token ? getUserBySession(token) : null
-  if (!user) redirect('/login')
   return <KanbanBoard />
 }
